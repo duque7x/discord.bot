38f321b63dad36f7ff90c4ddda7d03da9cb321ff
Dev_Josué <92229705+JosueHTML2@users.noreply.github.com>
2022-02-26 23:02:14 +0000

    Create help.js

---
+++ src/commands/info/help.js
@@ -0,0 +1,31 @@
+const { Interaction, BaseCommandInteraction, MessageEmbed } = require("discord.js");
+
+module.exports = class {
+   constructor(client) {
+      this.client = client;
+      this.name = 'help';
+      this.description = 'Embed de ajuda.'
+   }
+   /**
+    * 
+    * @param {BaseCommandInteraction} interaction 
+    */
+   run(interaction) {
+      const embed = new MessageEmbed()
+         .setColor('DARK_GREEN')
+         .setAuthor({ name: interaction.user.username, iconURL: interaction.user.displayAvatarURL() })
+         .setTitle("Embed de ajuda")
+         .setDescription("Embed de ajuda")
+         .addField("This is a single field title, it can hold 256 characters", "This is a field value, it can hold 1024 characters.")
+         .addFields(
+            { name: "Inline fields", value: "They can have different fields with small headlines, and you can inline them.", inline: true },
+            { name: "Masked links", value: "You can put [masked links](https://discord.js.org/#/docs/main/master/class/MessageEmbed) inside of rich embeds.", inline: true },
+            { name: "Markdown", value: "You can put all the *usual* **__Markdown__** inside of them.", inline: true }
+         )
+         .addField("\u200b", "\u200b")
+         .setTimestamp()
+         .setFooter({text: "This is the footer text, it can hold 2048 characters", iconURL: "http://i.imgur.com/w1vhFSR.png"});
+
+      interaction.reply({ embeds: [embed], ephemeral: true });
+   }
+}

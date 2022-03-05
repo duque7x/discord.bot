64f7da8ddc1079afd829d19a6559309aa55bdadb
Dev_Josué <92229705+JosueHTML2@users.noreply.github.com>
2022-03-05 17:13:08 +0000

    Adding intents

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const Bot = require("./src/structures/Client");
+const {Bot} = require("./src/structures/Client");
 const {token}= require('./config.json')
 const client = new Bot({
    intents: [
-      'GUILDS'
+      'GUILDS',
+      "GUILD_VOICE_STATES"
    ]
 });
 
 client.loadCommands();
 client.loadEvents();
 client.registryCommands()
+
+process.on('unhandledRejection', (err) => {});
+process.on('uncaughtException', (err) => {});

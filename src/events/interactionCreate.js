50d7c9aa6baa9fcae57bd0d186274498e94546f5
Dev_Josué <92229705+JosueHTML2@users.noreply.github.com>
2022-03-05 17:14:29 +0000

    T

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,6 +1,10 @@
 const { Interaction , BaseCommandInteraction} = require("discord.js");
-
+const { Bot } = require("../structures/Client");
 module.exports = class {
+   /**
+   * 
+   * @param {Bot} client 
+   */
    constructor(client) {
       this.client = client;
       this.name = 'interactionCreate'
@@ -10,13 +14,14 @@ module.exports = class {
     * @param {BaseCommandInteraction} interaction 
     */
    run(interaction) {
-      try  {
+      try {
          this.client.commands.find(i => i.name == interaction.commandName).run(interaction);
-      } catch {
+      } catch(o) {
          interaction.reply({
             content:  `O commando ${interaction.commandName} não foi encontrado!`,
             ephemeral: true,
          });
+         console.log(o);
       }
    }
 }
--- src/structures/Client.js
+++ src/structures/Client.js
@@ -1,22 +1,22 @@
 const { readdirSync } = require('fs')
-const { join } = require('path')
-const { Client } = require('discord.js')
+const { Client } = require('discord.js');
+const { DataBase } = require('@josue7/database');
 
-module.exports = class Bot extends Client {
+class Bot extends Client {
    constructor(options) {
       super(options)
-      this.commands = []
+      this.commands = [];
+      this.database = new DataBase();
    }
 
    async registryCommands() {
-      const {token}= require('../../config.json')
-       await this.login(token)
-      this.guilds.cache.get('947254393147887707').commands.set(this.commands)
-      //this.application.commands.set(this.commands)
+      const { token } = require('../../config.json');
+      await this.login(token);
+      this.guilds.cache.get('947254393147887707').commands.set(this.commands);
    }
 
    loadCommands(path = __dirname.replace('\\structures', '\\commands')) {
-     readdirSync(path).forEach((dir) => {
+      readdirSync(path).forEach((dir) => {
          const commands = readdirSync(`${path}\\${dir}`);
 
          for (const command of commands) {
@@ -25,20 +25,22 @@ module.exports = class Bot extends Client {
 
             this.commands.push(cmd);
          }
-     });
+      });
 
    }
    loadEvents(path = __dirname.replace('\\structures', '\\events')) {
       const categories = readdirSync(path);
 
       categories.forEach((file) => {
-         const eventClass =  new (require(`../events/${file}`))(this);
+         const eventClass = new (require(`../events/${file}`))(this);
          if (eventClass.name === 'ready') {
             return this.on('ready', (...a) => eventClass.run(...a));
          } else {
             return this.on(eventClass.name, (...a) => eventClass.run(...a));
          }
-        
+
       });
    }
 }
+
+module.exports.Bot = Bot;

c21bff0c253ace5c244f62734a01fb1f5a791934
Dev_Josué <92229705+JosueHTML2@users.noreply.github.com>
2022-03-10 19:59:44 +0000

    Adding intents and vulkava

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-const {Bot} = require("./src/structures/Client");
-const {token}= require('./config.json')
+const { Bot } = require("./src/structures/Client");
+const { token } = require('./config.json')
 const client = new Bot({
    intents: [
       'GUILDS',
-      "GUILD_VOICE_STATES"
+      "GUILD_VOICE_STATES",
+      "GUILD_MESSAGES"
    ]
 });
 
+client.vulkava.on('trackStart', (player, track) => {
+   const channel = client.channels.cache.get(player.textChannelId);
+
+   channel.send(`Tocando \`${track.title}\``);
+});
+
+client.vulkava.on('queueEnd', (player) => {
+   const channel = client.channels.cache.get(player.textChannelId);
+
+   channel.send(`Acabou!`);
+
+   player.destroy();
+});
+
+client.vulkava.on('error', (node, err) => {
+   console.error(`[Vulkava] Error on node ${node.identifier}`, err.message);
+});
 client.loadCommands();
 client.loadEvents();
 client.registryCommands()
-
-process.on('unhandledRejection', (err) => {});
-process.on('uncaughtException', (err) => {});
+client.host()
+process.on('unhandledRejection', (err) => { });
+process.on('uncaughtException', (err) => { });

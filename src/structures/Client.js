efc16da13963fad0ab9e724f14003fa77c9acb4e
Dev_Josué <92229705+JosueHTML2@users.noreply.github.com>
2022-03-10 20:01:24 +0000

    Update Client.js

--- src/structures/Client.js
+++ src/structures/Client.js
@@ -1,16 +1,33 @@
 const { readdirSync } = require('fs')
 const { Client } = require('discord.js');
 const { DataBase } = require('@josue7/database');
+const { Vulkava } = require('vulkava');
+const { token } = require('../../config.json');
+const express = require('express');
+const app = express();
 
 class Bot extends Client {
    constructor(options) {
       super(options)
       this.commands = [];
       this.database = new DataBase();
+      this.vulkava = new Vulkava({
+         nodes: [
+           {
+             id: 'Node 1',
+             hostname: '127.0.0.1',
+             port: 3000,
+           }
+         ],
+         sendWS: (guildId, payload) => {
+           this.guilds.cache.get(guildId)?.shard.send(payload);
+           // With eris
+           // client.guilds.get(guildId)?.shard.sendWS(payload.op, payload.d);
+         }
+       })
    }
 
    async registryCommands() {
-      const { token } = require('../../config.json');
       await this.login(token);
       this.guilds.cache.get('947254393147887707').commands.set(this.commands);
    }
@@ -34,12 +51,18 @@ class Bot extends Client {
       categories.forEach((file) => {
          const eventClass = new (require(`../events/${file}`))(this);
          if (eventClass.name === 'ready') {
-            return this.on('ready', (...a) => eventClass.run(...a));
+            return this.on('ready', (a) => eventClass.run(a));
          } else {
             return this.on(eventClass.name, (...a) => eventClass.run(...a));
          }
 
       });
+   }
+   host() {
+      app.get('/', (req, res) => {
+         res.sendStatus(202).send({ accepted: true });
+      });
+      app.listen(3000, console.log('Running.'));
    }
 }
 

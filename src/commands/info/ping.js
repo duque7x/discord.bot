18040c08bb1842d715d7c0c0915fb5cec674f55b
Dev_Josué <92229705+JosueHTML2@users.noreply.github.com>
2022-04-03 23:22:24 +0100

    Update ping.js

--- src/commands/info/ping.js
+++ src/commands/info/ping.js
@@ -16,19 +16,6 @@ module.exports = class {
          .setColor(0x3498DB)
          .setAuthor({ name: "Author Name, it can hold 256 characters", iconURL: "https://i.imgur.com/lm8s41J.png" })
          .setTitle("This is your title, it can hold 256 characters")
-         .setURL("https://discord.js.org/#/docs/main/stable/class/MessageEmbed")
-         .setDescription("This is the main body of text, it can hold 4096 characters.")
-         .setImage("http://i.imgur.com/yVpymuV.png")
-         .setThumbnail("http://i.imgur.com/p2qNFag.png")
-         .addField("This is a single field title, it can hold 256 characters", "This is a field value, it can hold 1024 characters.")
-         .addFields(
-            { name: "Inline fields", value: "They can have different fields with small headlines, and you can inline them.", inline: true },
-            { name: "Masked links", value: "You can put [masked links](https://discord.js.org/#/docs/main/master/class/MessageEmbed) inside of rich embeds.", inline: true },
-            { name: "Markdown", value: "You can put all the *usual* **__Markdown__** inside of them.", inline: true }
-         )
-         .addField("\u200b", "\u200b")
-         .setTimestamp()
-         .setFooter({text: "This is the footer text, it can hold 2048 characters", iconURL: "http://i.imgur.com/w1vhFSR.png"});
 
       interaction.reply({ embeds: [embed], ephemeral: true });
    }
